5b405c1a21d3d0749343cfd93e1d44337f68a966
saisandeep2484 <60545298+saisandeep2484@users.noreply.github.com>
2020-07-31 21:07:15 +0530

    Update parameters.py

--- sands/parameters.py
+++ sands/parameters.py
@@ -1,4 +1,4 @@
-search_query = 'site:linkedin.com/in OR site:linkedin.com/pub -intitle:profiles -inurl:"/dir AND "New York" AND "python developer"'
+
 result_file = 'output.csv'
 
 user = 'saisandeep2484@gmail.com'
